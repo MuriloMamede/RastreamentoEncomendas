bc3ae68f41f62205228b2eb174dc6a8dd0f049a4
MuriloMamede <muriloomamede@gmail.com>
2020-01-02 21:38:58 -0400

    Adding a new Interface with scaffold to the project

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
+import 'Objeto.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  String answer = "";
+  TextEditingController _trackingCodeController = TextEditingController();
+  void _track() async {
+
+    //ex.: AB123456789BR
+    String codigo = "lo457016692cn";
+
+    String envelope =
+        "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" "
+            +"xmlns:res=\"http://resource.webservice.correios.com.br/\"> "
+            +"   <soapenv:Header/> "
+            +"   <soapenv:Body>  "
+            +"      <res:buscaEventosLista> "
+            +"         <usuario>ECT</usuario> "
+            +"         <senha>SRO</senha>  "
+            +"         <tipo>L</tipo>  "
+            +"         <resultado>T</resultado>  "
+            +"         <lingua>101</lingua> "
+            +"         <objetos>"+codigo+"</objetos> "
+            +"      </res:buscaEventosLista>  "
+            +"   </soapenv:Body> "
+            +"</soapenv:Envelope>"
+    ;
+
+    final response =
+    await http.post("http://webservice.correios.com.br:80/service/rastro",
+      headers: {"Content-Type": "text/xml",
+      },body: envelope,);
+
+    Objeto objeto = Objeto(xml.parse(response.body).findAllElements("objeto"));
+
+
+    setState(() {
+      if(objeto.erro!=null){
+        answer ="Erro: "+objeto.erro;
+      }else{
+        answer =
+            "Número:"+objeto.numero+"\n"
+                +"Categoria:"+objeto.categoria+"\n"
+                +"Descrição:"+objeto.descricao+"\n"
+                +"Data:"+objeto.data+"\n"
+                +"Hora:"+objeto.hora+"\n";
+      }
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +78,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
               Padding(padding: EdgeInsets.only( bottom: 20),
                 child: TextField(
-                  keyboardType: TextInputType.number,
+                  keyboardType: TextInputType.text,
                   decoration: InputDecoration(
                     labelText: 'Insert your tracking code',
                   ),
@@ -39,8 +90,25 @@ class _HomeState extends State<Home> {
                       color: Colors.red
 
                   ),
+                  controller: _trackingCodeController,
                 ),
               ),
+              ButtonTheme(
+                minWidth: double.infinity,
+
+                child:RaisedButton(
+                  child: Text(
+                    "TRACK",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  color: Colors.amberAccent,
+
+                  onPressed: _track,
+                ),
+              ),
+                Text(answer)
               ],
             ),
           ),
